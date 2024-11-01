ee92439ca6981ba74075a9d5423dd0821090bc4f
Ali Hemmat <AliHemmat.dev@gmail.com>
2024-11-01 14:43:00 +0330

    fix: db connect to Supabase

---
+++ app/account/page.js
@@ -0,0 +1,9 @@
+function page() {
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Weolcome, Ali
+    </h2>
+  );
+}
+
+export default page;
---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import Header from "@/app/_components/Header";
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+export const metadata = {
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+  description:
+    "Luxurios cabin hotel, located in teh heart of the Italian Dolomites",
+};
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`bg-primary-950 antialiased text-primary-100 min-h-screen relative ${josefin.className} flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 p-12">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
